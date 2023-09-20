9f757e15fb662fd59e9a1f090aed6320c38f5e65
Gabriel <gszcode@gmail.com>
2023-09-19 23:49:53 -0300

    home agregado a app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <main className="main" id="home">
       <Header />
+      <Home />
     </main>
   )
 }
