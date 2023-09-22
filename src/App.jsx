b384cee6747589c964a83758dbed746823a0c1bf
Gabriel <gszcode@gmail.com>
2023-09-22 01:28:54 -0300

    componente about agregado a app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from './components/About/About'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 
@@ -6,6 +7,7 @@ function App() {
     <main className="main" id="home">
       <Header />
       <Home />
+      <About />
     </main>
   )
 }
