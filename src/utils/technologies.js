383f0764f00d535bf07eb8bb10a5bb9f2e6608f7
Gabriel <gszcode@gmail.com>
2023-09-21 22:10:45 -0300

    iconos de tecologias agregados

---
+++ src/utils/technologies.js
@@ -0,0 +1,52 @@
+export const technologies = [
+  {
+    id: 1,
+    tech: 'JavaScript',
+    icon: '/assets/icons/javascript.svg'
+  },
+  {
+    id: 2,
+    tech: 'TypeScript',
+    icon: '/assets/icons/typescript.svg'
+  },
+  {
+    id: 3,
+    tech: 'React.JS',
+    icon: '/assets/icons/react.svg'
+  },
+  {
+    id: 4,
+    tech: 'Next.js',
+    icon: '/assets/icons/next.svg'
+  },
+  {
+    id: 5,
+    tech: 'Tailwind',
+    icon: '/assets/icons/tailwind.svg'
+  },
+  {
+    id: 6,
+    tech: 'Node.js',
+    icon: '/assets/icons/node.svg'
+  },
+  {
+    id: 7,
+    tech: 'Express.js',
+    icon: '/assets/icons/express.svg'
+  },
+  {
+    id: 8,
+    tech: 'MongoDB',
+    icon: '/assets/icons/mongodb.svg'
+  },
+  {
+    id: 9,
+    tech: 'PostgreSQL',
+    icon: '/assets/icons/postgresql.svg'
+  },
+  {
+    id: 10,
+    tech: 'Git',
+    icon: '/assets/icons/git.svg'
+  }
+]
