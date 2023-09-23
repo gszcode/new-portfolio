5de559b7edec6e98d0d66841587209908585cb5a
Gabriel <gszcode@gmail.com>
2023-09-23 00:38:55 -0300

    componente article de project

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,41 @@
+import './Project.css'
+
+// eslint-disable-next-line react/prop-types
+function Project({ id, name, img, tech, url, repository }) {
+  return (
+    <article className="project" key={id}>
+      <h3 className="project_title">{name}</h3>
+      <div className="img_container">
+        <img src={img} alt={`${name} Project`} />
+      </div>
+      <div className="tech_project">
+        <div className="tech_used">
+          {/* eslint-disable-next-line react/prop-types */}
+          {tech.map((icon) => (
+            <img key={id} src={icon} alt={`${name} Icon`} />
+          ))}
+        </div>
+        <div className="project_link">
+          <a href={url} target="_blank" rel="noreferrer">
+            <img
+              src="public/assets/icons/globe.svg"
+              alt="Globe Icon"
+              title="Ver Proyecto"
+            />
+          </a>
+          <a href={repository} target="_blank" rel="noreferrer">
+            <img
+              src={`public/assets/icons/${
+                repository ? 'github.svg' : 'padlock.svg'
+              }`}
+              alt="GitHub Icon"
+              title="Ver Repositorio"
+            />
+          </a>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default Project
