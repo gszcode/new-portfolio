627c5bab2690a058f6a7c61972ef081d9f47f988
Gabriel <gszcode@gmail.com>
2023-09-22 22:42:44 -0300

    datos de proyectos creados

---
+++ src/utils/portfolio.js
@@ -0,0 +1,63 @@
+export const portfolio = [
+  {
+    id: 1,
+    name: 'Sharpness',
+    img: 'public/assets/img/project-05.png',
+    tech: [
+      '/assets/icons/typescript.svg',
+      '/assets/icons/next.svg',
+      '/assets/icons/css.svg'
+    ],
+    url: 'https://sharpness.vercel.app/',
+    repository: 'https://github.com/gszcode/sharpness'
+  },
+  {
+    id: 2,
+    name: 'Maular',
+    img: 'public/assets/img/project-04.png',
+    tech: [
+      '/assets/icons/javascript.svg',
+      '/assets/icons/next.svg',
+      '/assets/icons/sass.svg'
+    ],
+    url: 'https://www.maular.com/',
+    repository: ''
+  },
+  {
+    id: 3,
+    name: 'DewiPool',
+    img: 'public/assets/img/project-03.png',
+    tech: [
+      '/assets/icons/javascript.svg',
+      '/assets/icons/next.svg',
+      '/assets/icons/sass.svg'
+    ],
+    url: 'https://www.dewipool.com/',
+    repository: ''
+  },
+  {
+    id: 4,
+    name: 'Consultant',
+    img: 'public/assets/img/project-02.png',
+    tech: [
+      '/assets/icons/react.svg',
+      '/assets/icons/css.svg',
+      '/assets/icons/node.svg',
+      '/assets/icons/mongodb.svg'
+    ],
+    url: 'https://gscode.vercel.app/',
+    repository: 'https://github.com/gszcode/gscode'
+  },
+  {
+    id: 5,
+    name: 'PsyCare',
+    img: 'public/assets/img/project-01.png',
+    tech: [
+      '/assets/icons/javascript.svg',
+      '/assets/icons/react.svg',
+      '/assets/icons/css.svg'
+    ],
+    url: 'https://psycares.netlify.app/',
+    repository: 'https://github.com/gszcode/psycare'
+  }
+]
