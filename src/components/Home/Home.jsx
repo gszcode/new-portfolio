262f3ebb242d4d895a0ec8aa746faeeead9c2566
Gabriel Sanchez <gszcode@gmail.com>
2024-06-07 03:02:43 -0300

    CAMBIO foto de perfil

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,58 +1,46 @@
-import './Home.css'
-import profile from '/assets/profile.jpeg'
-import logo from '/assets/logo.png'
-import linkedin from '/assets/icons/linkedin.svg'
-import github from '/assets/icons/github.svg'
-import Skills from '../Skills/Skills'
+import "./Home.css";
+import profile from "/assets/profile.jpg";
+import logo from "/assets/logo.png";
+import linkedin from "/assets/icons/linkedin.svg";
+import github from "/assets/icons/github.svg";
+import Skills from "../Skills/Skills";
 
 function Home() {
-  return (
-    <section className="home_container">
-      <div className="home">
-        <div className="profile_image">
-          <img src={profile} alt="Foto Personal" />
-        </div>
-        <div className="main_logo">
-          <img src={logo} alt="Logo" />
-        </div>
-        <div className="introduction">
-          <h1>
-            Hola, Soy <span className="color">Gabriel Sanchez.</span>
-            <br />
-            Soy un entusiasta de la creación y el{' '}
-            <span className="color">Desarrollo Web</span>.
-            <br />
-            <br />
-          </h1>
-          <p>
-            Mi mayor interés es el <span className="color">Frontend</span> y es
-            donde mejor me desenvuelvo, aúnque también me gusta el{' '}
-            <span className="color">Backend</span>.
-          </p>
-          <span className="location color">Buenos Aires, Argentina.</span>
-        </div>
-        <div className="social">
-          <a
-            href="https://www.linkedin.com/in/gszcode/"
-            rel="noreferrer"
-            title="Linkedin"
-            target="_blank"
-          >
-            <img src={linkedin} alt="Linkedin Icon" />
-          </a>
-          <a
-            href="https://github.com/gszcode"
-            rel="noreferrer"
-            title="GitHub"
-            target="_blank"
-          >
-            <img src={github} alt="Github Icon" />
-          </a>
-        </div>
-      </div>
-      <Skills />
-    </section>
-  )
+	return (
+		<section className='home_container'>
+			<div className='home'>
+				<div className='profile_image'>
+					<img src={profile} alt='Foto Personal' />
+				</div>
+				<div className='main_logo'>
+					<img src={logo} alt='Logo' />
+				</div>
+				<div className='introduction'>
+					<h1>
+						Hola, Soy <span className='color'>Gabriel Sanchez.</span>
+						<br />
+						Soy un entusiasta de la creación y el <span className='color'>Desarrollo Web</span>.
+						<br />
+						<br />
+					</h1>
+					<p>
+						Mi mayor interés es el <span className='color'>Frontend</span> y es donde mejor me desenvuelvo, aúnque
+						también me gusta el <span className='color'>Backend</span>.
+					</p>
+					<span className='location color'>Buenos Aires, Argentina.</span>
+				</div>
+				<div className='social'>
+					<a href='https://www.linkedin.com/in/gszcode/' rel='noreferrer' title='Linkedin' target='_blank'>
+						<img src={linkedin} alt='Linkedin Icon' />
+					</a>
+					<a href='https://github.com/gszcode' rel='noreferrer' title='GitHub' target='_blank'>
+						<img src={github} alt='Github Icon' />
+					</a>
+				</div>
+			</div>
+			<Skills />
+		</section>
+	);
 }
 
-export default Home
+export default Home;
